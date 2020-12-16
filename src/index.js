f16dde60a1c1dc1f905b698884d78df43ec8473d
Yurii Grytsyk <grytsykyura@gmail.com>
2020-12-16 17:45:06 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./components/App";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./stylesheets/index.css";
+import "./stylesheets/index.scss";
+import { Provider } from 'react-redux';
+import store from './store/store';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
