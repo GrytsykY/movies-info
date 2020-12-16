80a4b976011eea70540cebb63cdefef0770a138b
Yurii Grytsyk <grytsykyura@gmail.com>
2020-12-16 17:39:30 +0200

    method DELETE - CallApi

--- src/api/api.js
+++ src/api/api.js
@@ -4,3 +4,81 @@ export const API_KEY_3 = "4237669ebd35e8010beee2f55fd45546";
 
 export const API_KEY_4 =
   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MjM3NjY5ZWJkMzVlODAxMGJlZWUyZjU1ZmQ0NTU0NiIsInN1YiI6IjVkMzc0NjVkZTU0ZDVkMjBhNGRiZDRlNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QkywZtcqMOTYX-fvvag8B-1DVXxaFEgVDumJkqV6oE0";
+
+  export const fetchApi = (url, options = {}) => {
+    return new Promise((resolve, reject) => {
+      fetch(url, options)
+        .then(response => {
+          if (response.status < 400) {
+            return response.json();
+          } else {
+            throw response;
+          }
+        })
+        .then(data => {
+          resolve(data);
+        })
+        .catch(response => {
+          response.json().then(error => {
+            reject(error);
+          });
+        });
+    });
+  };
+  
+  export default class CallApi {
+    static get(url, options = {}) {
+      const { params = {} } = options;
+      const queryStringParams = {
+        api_key: API_KEY_3,
+        ...params
+      };
+      return fetchApi(
+        `${API_URL}${url}?${queryString.stringify(queryStringParams)}`,
+        {
+          mode: "cors",
+          headers: {
+            "Content-type": "application/json"
+          }
+        }
+      );
+    }
+  
+    static post(url, options = {}) {
+      const { params = {}, body = {} } = options;
+      const queryStringParams = {
+        api_key: API_KEY_3,
+        ...params
+      };
+      return fetchApi(
+        `${API_URL}${url}?${queryString.stringify(queryStringParams)}`,
+        {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            "Content-type": "application/json"
+          },
+          body: JSON.stringify(body)
+        }
+      );
+    }
+  
+    static delete(url, options = {}) {
+      const { params = {}, body = {} } = options;
+      const queryStringParams = {
+        api_key: API_KEY_3,
+        ...params
+      };
+      return fetchApi(
+        `${API_URL}${url}?${queryString.stringify(queryStringParams)}`,
+        {
+          method: "DELETE",
+          mode: "cors",
+          headers: {
+            "Content-type": "application/json"
+          },
+          body: JSON.stringify(body)
+        }
+      );
+    }
+  }
